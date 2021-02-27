c57d1cf0bd5252844b664c9c46d36f42104bc37d
MilesCranmer <miles.cranmer@gmail.com>
2021-02-26 23:52:55 -0500

    Working tests for JAX

--- tests/test_some.py
+++ tests/test_some.py
@@ -1,51 +1,33 @@
-import sympy, torch, sympytorch
+import numpy as np
+from jax import numpy as jnp
+from jax import random
+from jax import grad
+import sympy
+from sympy2jax import sympy2jax
 
+x, y, z = sympy.symbols('x y z')
 
 def test_example():
-    x = sympy.symbols('x_name')
     cosx = 1.0 * sympy.cos(x)
-    sinx = 2.0 * sympy.sin(x)
-
-    mod = sympytorch.SymPyModule(expressions=[cosx, sinx])
-    
-    x_ = torch.rand(3)
-    out = mod(x_name=x_)  # out has shape (3, 2)
-
-    assert torch.equal(out[:, 0], x_.cos())
-    assert torch.equal(out[:, 1], 2 * x_.sin())
-    assert out.requires_grad  # from the two Parameters initialised as 1.0 and 2.0
-    assert {x.item() for x in mod.parameters()} == {1.0, 2.0}
-
+    key = random.PRNGKey(0)
+    X = random.normal(key, (1000, 1))
+    true = 1.0 * jnp.cos(X[:, 0])
+    f, params = sympy2jax(cosx, [x])
+    assert jnp.all(jnp.isclose(f(X, params), true)).item()
 
 def test_grad():
-    x = sympy.symbols('x_name')
-    y = 1.0 * x
-    mod = sympytorch.SymPyModule(expressions=[y])
-    out = mod(x_name=torch.ones(()))
-    out.backward()
-    with torch.no_grad():
-        for param in mod.parameters():
-            param += param.grad
-    expr, = mod.sympy()
-    assert expr == 2.0 * x
+    cosx = 1.0 * sympy.cos(x)
+    key = random.PRNGKey(0)
+    X = random.normal(key, (1000, 1))
+    true_grad = - 1.0 * jnp.sin(X[:, 0])
+    f, params = sympy2jax(cosx, [x])
+    assert jnp.all(jnp.isclose(grad(f(X, params), params), true)).item()
 
 
-def test_reduce():
-    x, y = sympy.symbols('x y')
-    z = 2 * x * y
-    mod = sympytorch.SymPyModule(expressions=[z])
-    mod(x=torch.rand(2), y=torch.rand(2))
-
-    z = 2 + x + y
-    mod = sympytorch.SymPyModule(expressions=[z])
-    mod(x=torch.rand(2), y=torch.rand(2))
-
-
-def test_special_subclasses():
-    x, y = sympy.symbols('x y')
-    z = x - 1
-    w = y * 0
-
-    mod = sympytorch.SymPyModule(expressions=[z, w])
-    assert mod.sympy() == [x - 1, sympy.Integer(0)]
-
+def test_multiple():
+    cosxyz = 1.0 * sympy.cos(x) - 3.2 * sympy.Abs(y)**z
+    key = random.PRNGKey(0)
+    X = random.normal(key, (1000, 3))
+    true = 1.0 * jnp.cos(X[:, 0]) - 3.2 * jnp.abs(X[:, 1])**X[:, 2]
+    f, params = sympy2jax(cosxyz, [x, y, z])
+    assert jnp.all(jnp.isclose(f(X, params), true)).item()
