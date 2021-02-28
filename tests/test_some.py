33d99a65d8112f0154764c24b0b450b5b30e7332
MilesCranmer <miles.cranmer@gmail.com>
2021-02-28 05:11:52 -0500

    Make tests run for main

--- tests/test_some.py
+++ tests/test_some.py
@@ -1,7 +1,7 @@
 import numpy as np
 from jax import numpy as jnp
 from jax import random
-from jax import grad
+from jax import grad, jacobian
 import sympy
 from sympy2jax import sympy2jax
 
@@ -10,7 +10,7 @@ x, y, z = sympy.symbols('x y z')
 def test_example():
     cosx = 1.0 * sympy.cos(x)
     key = random.PRNGKey(0)
-    X = random.normal(key, (1000, 1))
+    X = random.normal(key, (100, 1))
     true = 1.0 * jnp.cos(X[:, 0])
     f, params = sympy2jax(cosx, [x])
     assert jnp.all(jnp.isclose(f(X, params), true)).item()
@@ -18,21 +18,24 @@ def test_example():
 def test_grad():
     cosx = 1.0 * sympy.cos(x)
     key = random.PRNGKey(0)
-    X = random.normal(key, (1000, 1))
+    X = random.normal(key, (100, 1))
     true_grad = - 1.0 * jnp.sin(X[:, 0])
     f, params = sympy2jax(cosx, [x])
-    assert jnp.all(jnp.isclose(grad(f(X, params), params), true)).item()
+    grad_prediction = grad(lambda x: f(x[:, None], params).sum())(X[:, 0])
+    assert jnp.all(jnp.isclose(grad_prediction, true_grad)).item()
 
 
 def test_multiple():
     cosxyz = 1.0 * sympy.cos(x) - 3.2 * sympy.Abs(y)**z
     key = random.PRNGKey(0)
-    X = random.normal(key, (1000, 3))
+    X = random.normal(key, (100, 3))
     true = 1.0 * jnp.cos(X[:, 0]) - 3.2 * jnp.abs(X[:, 1])**X[:, 2]
     f, params = sympy2jax(cosxyz, [x, y, z])
     assert jnp.all(jnp.isclose(f(X, params), true)).item()
 
-def main():
-    test_example()
-    test_grad()
-    test_multiple()
+print("Test 1")
+test_example()
+print("Test 2")
+test_grad()
+print("Test 3")
+test_multiple()
